e00fc49ab34a9aaa5df2354ce704f4c8b8435f4f
zazu7765 <ohmyzazu@gmail.com>
2023-05-06 00:40:47 -0400

    update source

---
+++ src/main.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"database/sql"
+	"errors"
+	"fmt"
+	"log"
+	_ "modernc.org/sqlite"
+	"os"
+	"path/filepath"
+)
+
+type CardItem struct {
+	Question string
+	Options  []string
+	Answer   string
+}
+
+type Quiz int64
+
+const (
+	Undefined Quiz = iota
+	Card
+	MCQ
+)
+
+func (q Quiz) String() string {
+	switch q {
+	case Card:
+		return "Card"
+	case MCQ:
+		return "MCQ"
+	}
+	return "unknown"
+}
+
+var ROOT string
+
+func createDeck(n string, t Quiz) error {
+	dbName := ROOT + "/" + t.String() + "_" + n + ".db"
+	if _, err := os.Stat(dbName); os.IsExist(err) {
+		return errors.New("Quiz already exists")
+	}
+	db, err := sql.Open("sqlite", dbName)
+	if err != nil {
+		return err
+	}
+	db.Exec("CREATE TABLE cards (id INT primary key, question TEXT not null, answer TEXT not null);")
+	defer db.Close()
+	return nil
+}
+
+func parseDeck(n string) (*sql.DB, error) {
+	file := ROOT + "/" + n
+	db, err := sql.Open("sqlite", file)
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
+}
+func main() {
+	home, err := os.UserHomeDir()
+	if err != nil {
+		log.Fatal(err)
+		os.Exit(1)
+	}
+	// define root directory for all operations
+	ROOT = home + "/.quiz"
+	if _, err := os.Stat(ROOT); os.IsNotExist(err) {
+		os.Mkdir(ROOT, 0750)
+	}
+	createDeck("banana", Card)
+	files, err := os.ReadDir(ROOT)
+	for _, file := range files {
+		fmt.Println(file.Name())
+		if filepath.Ext(file.Name()) == ".db" {
+			db, err := parseDeck(file.Name())
+			defer db.Close()
+			if err != nil {
+				fmt.Println(err)
+			}
+			stmt, err := db.Prepare("insert into cards (question, answer) values (?,?)")
+			if err != nil {
+				log.Fatal(err)
+			}
+			defer stmt.Close()
+			_, err = stmt.Exec("who is the current president of the united states", "joe biden")
+			rows, err := db.Query("select question,answer from cards")
+			if err != nil {
+				log.Fatal(err)
+			}
+			defer rows.Close()
+			for rows.Next() {
+				var q, a string
+				if err := rows.Scan(&q, &a); err != nil {
+					log.Fatal(err)
+				}
+				fmt.Println(q, a)
+			}
+		}
+	}
+}
